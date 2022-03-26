78ecb56becf0612b989736f9ca2d0ab2bc92dff1
Amanda Smith <ajs.amandasmith@gmail.com>
2022-03-25 21:23:01 -0500

    commit tree 4bae86f7cfaf9ed15ceaa7b8a5e69c34ab9d4297

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // event.target.children[1].value; -- One way to get the value we need from the event object
+    const input = document.querySelector('input#searchByID'); /* another way to get the value from the event object */
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
